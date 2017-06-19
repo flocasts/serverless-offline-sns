f3e48b1e199dcd4a1953f160c5c056556b1bc9b4
Karl Snyder <karlsnyder0@gmail.com>
2017-06-19 16:07:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 'use strict';
 
-var server = require('./lib/server');
+const server = require('./lib/server');
 
 class OfflineSNS {
-    constructor (serverless, options) {
+    constructor(serverless, options) {
         this.serverless = serverless;
         this.options = options;
-        const start = this.start.bind(this);
+        this.provider = this.serverless.getProvider('aws');
+        this.start = this.start.bind(this);
 
         this.hooks = {
-            'offline:start:init': start,
-            'offline:start': start
+            'offline:start:init': this.start.bind(this),
+            'offline:start': this.start.bind(this),
+            'offline:start:end': this.end.bind(this),
         };
     }
 
-    start () {
+    start() {
         server.create(this.serverless, this.options);
         server.parseSLSConfig(this.serverless);
         return server.listen();
+    }
+
+    end() {
+        server.close()
+            .then(() => process.exit(this.exitCode));
     }
 }
 
